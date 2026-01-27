e966aac0e4a9f9d1478e425684a045b8cfd1a4fb
limjeahyuk <lim0202jh@gmail.com>
2026-01-27 17:33:18 +0900

    feat: 1/27 update

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -4,22 +4,40 @@ const rooms = [
   { id: "general", title: "기본" },
   { id: "dev", title: "실험" },
   { id: "ai", title: "AI" },
+  { id: "bonono", title: "ㅇㅇㅇㅇ" },
+];
+
+const generalMessages = [
+  { role: "other", content: "안녕", status: "done" },
+  { role: "me", content: "Hello", status: "done" },
+  { role: "other", content: "맛집 추천", status: "done" },
+  { role: "me", content: "나도 몰라", status: "done" },
+  { role: "other", content: "안녕", status: "done" },
+  { role: "me", content: "안녕", status: "done" },
+  { role: "me", content: "안녕", status: "done" },
+  { role: "other", content: "안녕", status: "done" },
 ];
 
 // 에러 데모용: 20% 확률로 실패
-// const maybeFail = () => Math.random() < 0.2;
+const maybeFail = () => Math.random() < 0.2;
 
 export const handlers = [
   http.get("/api/rooms", async () => {
     await delay(1200);
 
-    // if (maybeFail()) {
-    //   return HttpResponse.json(
-    //     { message: "Failed to load rooms (mock error)" },
-    //     { status: 500 },
-    //   );
-    // }
-
     return HttpResponse.json({ rooms });
+  }),
+
+  http.get("/api/general", async () => {
+    await delay(2000);
+
+    if (maybeFail()) {
+      return HttpResponse.json(
+        { message: "Failed to load rooms (mock error)" },
+        { status: 500 },
+      );
+    }
+
+    return HttpResponse.json({});
   }),
 ];
